c8d302862612d7ede5c40005afcf63b71a7fcbf4
Jonathan Helmus <jjhelmus@gmail.com>
2016-04-13 15:51:17 -0500

    Update cvxopt_glpk recipe

--- cvxopt_glpk/build.sh
+++ cvxopt_glpk/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+export BLAS_LAPACK_LIB_PATHS=$PREFIX/lib
+if [ "$(uname)" == "Linux" ]; then
+    export CVXOPT_BLAS_LIB=openblas
+    export CVXOPT_LAPACK_LIB=openblas
+    export CVXOPT_BLAS_EXTRA_LINK_ARGS=-lgfortran
+fi
+export CFLAGS="-I$PREFIX/include -L$PREFIX/lib"
 CVXOPT_BUILD_GLPK=1 ${PYTHON} setup.py install
